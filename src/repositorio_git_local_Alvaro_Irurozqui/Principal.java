921650e7886fcd1eaaf2a207a116c3676972b4a7
alvaro <alvarois05@educastur.es>
2022-03-17 21:22:32 +0100

    segunda actualizacion del programa

--- src/repositorio_git_local_Alvaro_Irurozqui/Principal.java
+++ src/repositorio_git_local_Alvaro_Irurozqui/Principal.java
@@ -14,37 +14,59 @@ public class Principal {
 		System.out.println("2-Restar");
 		System.out.println("3-Multiplicar");
 		System.out.println("4-Dividir");
+		System.out.println("5-Potenciacion");
+		opcionMenu = teclado.nextInt();
 		while(opcionMenu != -2) {
-			if(opcionMenu == 1) {
+			switch (opcionMenu) {
+			case 1:
 				sumar(operacion1);
-			}else if(opcionMenu == 2){
+				break;
+			case 2:
 				restar(operacion1);
-			}else if(opcionMenu == 3) {
+				break;
+			case 3:
 				multiplicar(operacion1);
-			}else if(opcionMenu == 4) {
+				break;
+			case 4:
 				dividir(operacion1);
+				break;
+			case 5:
+				potenciacion(operacion1);
+				break;
 			}
+			System.out.println("---Selecciona la operacion---");
+			System.out.println("1-Sumar");
+			System.out.println("2-Restar");
+			System.out.println("3-Multiplicar");
+			System.out.println("4-Dividir");
+			System.out.println("5-Potenciacion");
+			opcionMenu = teclado.nextInt();
 		}
 		teclado.close();
 	}
-	public static double sumar(Operacion operacion1) {
-		double sumar;
-		sumar = operacion1.getA() + operacion1.getB();
-		return sumar;
+	public static void sumar(Operacion operacion1) {
+		double resultado;
+		resultado = operacion1.getA() + operacion1.getB();
+		System.out.println(resultado);
 	}
-	public static double restar(Operacion operacion1) {
+	public static void restar(Operacion operacion1) {
 		double restar;
 		restar = operacion1.getA() + operacion1.getB();
-		return restar;
+		System.out.println(restar);
 	}
-	public static double multiplicar(Operacion operacion1) {
+	public static void multiplicar(Operacion operacion1) {
 		double multiplicacion;
 		multiplicacion = operacion1.getA() + operacion1.getB();
-		return multiplicacion;
+		System.out.println(multiplicacion);
 	}
-	public static double dividir(Operacion operacion1) {
+	public static void dividir(Operacion operacion1) {
 		double division;
 		division = operacion1.getA() + operacion1.getB();
-		return division;
+		System.out.print(division);;
+	}
+	public static void potenciacion(Operacion operacion1) {
+		double potencia;
+		potencia = operacion1.getA()*operacion1.getA() + operacion1.getB()*operacion1.getB();
+		System.out.println(potencia);
 	}
 }
